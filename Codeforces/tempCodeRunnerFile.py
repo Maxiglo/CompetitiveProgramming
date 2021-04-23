f0c917046d98f192b6d51a5a2338c97da898222d
Maxime Gloesener <max.gleu@gmail.com>
2021-04-23 23:38:08 +0200

    23

--- Codeforces/tempCodeRunnerFile.py
+++ Codeforces/tempCodeRunnerFile.py
@@ -1,34 +1,6 @@
-s = input()
+a,b=map(int,input().split())
 
-
-if ('h' and 'e' and 'l' and 'l' and 'o' not in s) or len(s)<5:
-    print('NO')
-    exit()
-
-pos= s.index('h')
-s= s[pos:]
-if 'e' not in s:
-    print('NO')
-    exit()
-else:
-    pos=s.index('e')
-
-s= s[pos:]
-if 'l' not in s:
-    print('NO')
-    exit()
-else:
-    pos=s.index('l')+1
-s = s[pos:]
-
-if 'l' not in s:
-    print('NO')
-    exit()
-else:
-    pos=s.index('l')
-s = s[pos:]
-
-if 'o' not in s:
-    print('NO')
-else:
-    print('YES')
+ans=1
+for j in range(1,min(a,b)+1):
+    ans*=j
+print(ans)
