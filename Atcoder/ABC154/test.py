aba37206c80c6b8cb87bfaaed1cc8b2eef13a26f
Maxime Gloesener <max.gleu@gmail.com>
2021-05-12 19:29:50 +0200

    abc200A

--- Atcoder/ABC154/test.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def solve(self, n):
-        n=list(str(n))
-        for c in range(len(n)):
-            if int(c)<3:
-                n[c]='3'
-                break
-        return(int(''.join(n)))
