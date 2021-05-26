3dd51c53b9240a509c14f8101b13aa13f713e997
Maxime Gloesener <max.gleu@gmail.com>
2021-05-26 16:18:33 +0200

    ez

---
+++ Kattis/magictrick.py
@@ -0,0 +1,5 @@
+s=input()
+if len(set(s))==len(s):
+    print(1)
+else:
+    print(0)
--- Kattis/ratingproblem.py
+++ Kattis/ratingproblem.py
@@ -1,1 +1,7 @@
-print('test')
+n,k=map(int,input().split())
+somme=0
+for i in range(k):
+    somme+=int(input())
+m=(n-k)*3+somme
+mn=(n-k)*(-3)+somme
+print(mn/n,m/n)
