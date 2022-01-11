8dea9d82ec4d21fa7432de4806fc5c2a0aa36f58
Maxime Gloesener <max.gleu@gmail.com>
2022-01-11 02:38:49 +0100

    training

---
+++ Codeforces/A_Remove_Smallest.py
@@ -0,0 +1,15 @@
+for _ in range(int(input())):
+    x = int(input())
+    l = sorted(list(set([int(i) for i in input().split()])))
+    if len(l) == 0:
+        print('YES')
+        continue
+    mdiff=0
+    ok = 0
+    for i in range(len(l)-1):
+        if l[i+1]-l[i] > 1:
+            print('NO')
+            ok=1
+            break
+    if not ok:
+        print('YES')
