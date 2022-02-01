f0cf63620aa7c55d23700ea33a365a74541f7ee3
MaximeGloesener <30629620+MaximeGloesener@users.noreply.github.com>
2022-02-02 00:41:02 +0100

    Delete Advent of code 2020 directory

--- Advent of code 2020/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-    c=0
-    for line in fin:
-        line=line.split()
-        a=int(line[0].split('-')[0])
-        b=int(line[0].split('-')[1])
-        lettre=line[1][0]
-        if a<=line[2].count(lettre)<=b:
-            c+=1
-    print(c)
