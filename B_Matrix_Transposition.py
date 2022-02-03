2bee5678143bbda4110732ef90fbf957814b9c25
Maxime Gloesener <max.gleu@gmail.com>
2022-02-04 00:38:43 +0100

    atcoder

---
+++ B_Matrix_Transposition.py
@@ -0,0 +1,12 @@
+a,b = map(int,input().split())
+
+l =[]
+for _ in range(a):
+    l.append(input().split())
+
+for i in range(b):
+    rep = []
+    for j in range(a):
+        rep.append(l[j][i])
+    print(*rep)
+
